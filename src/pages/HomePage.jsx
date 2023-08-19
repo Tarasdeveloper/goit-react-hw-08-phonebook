fae64470bdd2b6e86680f0620912e9961dcf885e
Tarasdeveloper <tomahavki1@gmail.com>
2023-08-19 21:28:33 +0300

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,7 +4,7 @@ import Wrapper from 'components/Wrapper/Wrapper';
 const HomePage = () => {
   return (
     <Wrapper>
-      <h1>Welcome, this is my phonebook log in to use</h1>
+      <h1>Welcome, this is my phonebook, log in to use</h1>
     </Wrapper>
   );
 };
