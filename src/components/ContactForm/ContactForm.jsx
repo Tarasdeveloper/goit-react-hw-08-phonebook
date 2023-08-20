e14cd38390e3ea15e669f043c2dc62c25bcb7f6b
Tarasdeveloper <tomahavki1@gmail.com>
2023-08-20 11:36:06 +0300

    Update ContactForm.jsx

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   Form,
   FormButton,
@@ -17,6 +17,11 @@ export default function ContactForm() {
   const contacts = useSelector(selectUserContacts);
   const dispatch = useDispatch();
 
+  const [contactData, setContactData] = useState({
+    contactName: '',
+    contactNumber: '',
+  });
+
   useEffect(() => {
     dispatch(requestContactsThunk());
   }, [dispatch]);
@@ -28,9 +33,23 @@ export default function ContactForm() {
     const name = form.elements.contactName.value;
     const number = form.elements.contactNumber.value;
 
-    if (contacts.some(contact => contact.name === name))
-      return alert(`Contact with name ${name} already exists!`);
-    dispatch(addContactThunk({ name, number }));
+    if (contacts.some(contact => contact.name === name)) {
+      alert(`Contact with name ${name} already exists!`);
+    } else {
+      dispatch(addContactThunk({ name, number }));
+      setContactData({
+        contactName: '',
+        contactNumber: '',
+      });
+    }
+  };
+
+  const handleInputChange = event => {
+    const { name, value } = event.target;
+    setContactData(prevData => ({
+      ...prevData,
+      [name]: value,
+    }));
   };
 
   return (
@@ -42,6 +61,8 @@ export default function ContactForm() {
           name="contactName"
           // pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          value={contactData.contactName}
+          onChange={handleInputChange}
           required
         />
       </FormLabel>
@@ -52,6 +73,8 @@ export default function ContactForm() {
           name="contactNumber"
           // pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          value={contactData.contactNumber}
+          onChange={handleInputChange}
           required
         />
       </FormLabel>
